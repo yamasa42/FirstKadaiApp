472e35902c5fe67f2846eba41ad98e05e2bcafb8
sato.yama <imdc@ymail.plala.or.jp>
2019-11-17 19:07:57 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by imdc-yamada on 2019/11/17.
+//  Copyright © 2019 imdc-yamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
